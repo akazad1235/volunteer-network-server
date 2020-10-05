4469414b522773d938bec1aee8dde61effe99fdb
akazad1235 <akazad914@gmail.com>
2020-10-05 19:02:34 +0600

    dotenv file added

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const pass = "volunteer12345";
+require('dotenv').config();
 
 
 
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectId;
-const uri = "mongodb+srv://volunteer:volunteer12345@cluster0.e0dyf.mongodb.net/volunteer-network?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e0dyf.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const newUserCollection = client.db("volunteer-network-").collection("volunteer");
@@ -37,7 +37,6 @@ client.connect(err => {
   })
 
   app.delete('/delete/:id', (req, res) => {
-   // console.log(ObjectId(req.params.id));
     newUserCollection.deleteOne({_id: ObjectId(req.params.id)})
     .then( result => {
       console.log(result);
@@ -52,7 +51,6 @@ client.connect(err => {
   })
   //delete registerd person
   app.delete('/deleteRegPerson/:id', (req, res) => {
-    // console.log(ObjectId(req.params.id));
      newUserCollection.deleteOne({_id: ObjectId(req.params.id)})
      .then( result => {
        console.log(result);
