5831e03db7f0fed1cc796b1c256f8a4b847e9f49
akazad1235 <akazad914@gmail.com>
2020-10-06 08:26:55 +0600

    port update

--- index.js
+++ index.js
@@ -2,19 +2,12 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config()
-const port = 4000;
+const port = 4200;
 
 const app = express();
 
-
-
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-
-
-
 
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectId;
